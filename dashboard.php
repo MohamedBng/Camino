f59783041a5c726644b5f83d29d835b681d7eb88
Mohamed Bengrich <77544040+MohamedBng@users.noreply.github.com>
2022-06-13 10:43:28 +0200

    Add files via upload

---
+++ dashboard.php
@@ -0,0 +1,45 @@
+<?php
+require_once('autoload.php');
+?>
+<!doctype html>
+  <html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="shortcut icon" href="img/camino_logo.jpg" type="image/x-icon">
+    <title>Camino TV</title>
+  </head>
+  <body>
+    <nav id="menu">
+      <img class="logo" src="img/camino_logo.jpg" alt="logo">
+      <ul>
+        <li>
+          <a href="index.php">Site</a>
+        </li>
+      </ul>
+
+
+    </nav>
+    <main>
+      <section class="corp">
+        <div class="corpHeader">
+          <p class="title">Panneau de controle</p>
+        </div>
+
+        <form method="post" >
+          <div class="allItems">
+            <?php
+            $item = new Item;
+            $item->afficher();
+
+          ?></div>
+          <button id="submit" type="submit" name="save">Save</button>
+        </form>
+      </section>
+    </main>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+  </body>
+  </html>
---
+++ updateForm.php
@@ -0,0 +1,31 @@
+<?php
+require_once('autoload.php');
+$id = $_GET['id'];
+?>
+<!doctype html>
+  <html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+
+    <link rel="shortcut icon" href="img/camino_logo.jpg" type="image/x-icon">
+    <title>Camino TV</title>
+  </head>
+  <body>
+   <div>
+    <img class="logoUpdate" src="img/camino_logo.jpg" alt="logo">
+    
+
+
+    <form enctype="multipart/form-data" action="../Camino/php/Item.php" method="post" class="formUpdate">
+      <p>Nouveau Titre</p>
+      <input type="text" name="titre" placeholder="Nouveaux Titre">
+      <input type="hidden" name="id" value= <?php echo $id  ?> >
+      <button type="submit" name="update" value="update">Save</button>
+    </form>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  </div>
+</body>
+</html>
